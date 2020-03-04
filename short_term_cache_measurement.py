d0797020d79d2d82b1200834e57b40e8969998dd
Oleksii Prosianko <oleksii.prosianko@fedex.com>
2020-03-04 17:42:29 +0100

    short term measurements: row data added

---
+++ short_term_cache_measurement.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(datetime(2017, 11, 28, 23, 55))
+
+# date, hits * 1e3, mis * 1e3
+row_data = [
+    (datetime(2020, 2, 17, 15, 18),   1,  18),
+    (datetime(2020, 2, 17, 15, 30),   4,  42),
+    (datetime(2020, 2, 17, 16,  0),   8,  72),
+    (datetime(2020, 2, 17, 16, 30),  17, 119),
+    (datetime(2020, 2, 17, 17,  0),  30, 155),
+    (datetime(2020, 2, 17, 17, 30),  48, 195),
+    (datetime(2020, 2, 17, 18,  0),  69, 235),
+    (datetime(2020, 2, 17, 19,  0), 123, 315),
+    (datetime(2020, 2, 17, 20,  0), 184, 389),
+    (datetime(2020, 2, 17, 21,  0), 260, 451),
+    (datetime(2020, 2, 17, 22,  0), 314, 484),
+    (datetime(2020, 2, 17, 22, 40), 392, 530),
+    (datetime(2020, 2, 17, 23, 52), 412, 540),
+    (datetime(2020, 2, 17, 23, 55), 455, 558),
+    (datetime(2020, 2, 18,  0, 30), 535, 593),
+    (datetime(2020, 2, 18,  1, 20), 566, 616),
+    (datetime(2020, 2, 18,  2, 10), 646, 644),
+    (datetime(2020, 2, 18,  3, 20), 736, 680)
+]
+
+print(row_data)
