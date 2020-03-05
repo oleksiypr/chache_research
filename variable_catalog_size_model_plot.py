b41d18dab3ea30610b135a754f8e8640aa6390e3
Oleksii Prosianko <oleksii.prosianko@fedex.com>
2020-03-05 17:29:00 +0100

    variable_catalog_size_model_plot.py: S added

--- variable_catalog_size_model_plot.py
+++ variable_catalog_size_model_plot.py
@@ -6,33 +6,41 @@ init_printing()
 
 _lambda, kappa, t, K0 = symbols('lambda kappa t K_0', positive=True)
 
-H = \
-    -(_lambda * K0 / (kappa + _lambda)) \
-    + _lambda / (kappa + _lambda) \
-    * K0 ** (1 + _lambda / kappa) \
-    * (K0 + kappa * t) ** (-_lambda / kappa) \
+H = -(_lambda * K0 / (kappa + _lambda)) \
+    + _lambda/(kappa + _lambda)\
+        * K0**(1 + _lambda/kappa)\
+        * (K0 + kappa * t)**(-_lambda / kappa) \
     + _lambda ** 2 * t / (kappa + _lambda)
 
+S = - (_lambda/(kappa + _lambda)) \
+        * K0**(1 + _lambda/kappa) \
+        * (K0 + kappa*t)**(-_lambda/kappa) \
+    + (_lambda/(kappa + _lambda)) * (K0 + kappa*t)
+
 pretty_print(Eq(Function('H')(t), H))
+pretty_print(Eq(Function('S')(t), S))
 
 func_H = lambdify([t, _lambda, kappa, K0], H)
+func_S = lambdify([t, _lambda, kappa, K0], S)
 
-_lambda = 0.11
-kappa = 0.06
-K0 = 0.3
+_lambda = 1
+kappa = 0.1
+K0 = 0.7
 
 
 def calc_hits(t):
     return func_H(t, _lambda, kappa, K0)
 
 
-t = np.arange(0., 40., 0.05)
-plt.ylim(0, 3)
+def calc_miss(t):
+    return func_S(t, _lambda, kappa, K0)
 
+
+t = np.arange(0., 3., 0.05)
 hits = calc_hits(t)
-actual_t = [6.6, 11.0, 14.0, 20.0, 33.0]
-actual_H = [0.3, 0.6, 0.8, 1.3, 2.2]
+miss = calc_miss(t)
 
-plt.plot(t, hits, t, 0.11 * t, '--', actual_t, actual_H, 'ro')
-
+plt.plot(t, hits, color = 'b')
+plt.plot(t, miss, color = 'g')
+plt.plot(t, hits + miss, color = 'r')
 plt.show()
