2a1399674401ce6e18fff19a20cbd1f375ed29cb
Oleksii Prosianko <oleksii.prosianko@fedex.com>
2020-03-06 21:13:43 +0100

    long_term_cache_measurement.py implemented

--- long_term_cache_measurement.py
+++ long_term_cache_measurement.py
@@ -43,15 +43,15 @@ def to_hours(dt):
     return dt.days*24 + dt.seconds/3600
 
 
-time = [to_hours(r[0] - t0) for r in row_data]
-hits = [r[1] / 1000.0 for r in row_data]
-miss = [r[2] / 1000.0 for r in row_data]
+time = [to_hours(r[0] - t0) /10.0 for r in row_data]
+hits = [r[1]/1000.0 for r in row_data]
+miss = [r[2]/1000.0 for r in row_data]
 n    = [hits[i] + miss[i] for i in np.arange(size)]
 
 plt.plot(time, hits, 'ro', color = 'b')
 plt.plot(time, miss, 'ro', color = 'g')
 plt.plot(time,    n, 'bs', color = 'r')
 
-plt.xlabel('time, hours')
+plt.xlabel('time, 10 hours')
 plt.ylabel('count, 10^6')
 plt.show()
