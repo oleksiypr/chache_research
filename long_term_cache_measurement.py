7b4f1693ada54bff43de90b0edfd5f860d55e9bc
Oleksii Prosianko <oleksii.prosianko@fedex.com>
2020-03-11 12:12:43 +0100

    long_term_cache_measurement.py: short + long term data handled

--- long_term_cache_measurement.py
+++ long_term_cache_measurement.py
@@ -6,6 +6,28 @@ from sympy import pretty_print, init_printing
 init_printing()
 
 # date, hits * 1e3, mis * 1e3
+short_term_raw_data = [
+    (datetime(2020, 2, 17, 15, 18),   1,  18),
+    (datetime(2020, 2, 17, 15, 30),   4,  42),
+    (datetime(2020, 2, 17, 16,  0),   8,  72),
+    (datetime(2020, 2, 17, 16, 30),  17, 119),
+    (datetime(2020, 2, 17, 17,  0),  30, 155),
+    (datetime(2020, 2, 17, 17, 30),  48, 195),
+    (datetime(2020, 2, 17, 18,  0),  69, 235),
+    (datetime(2020, 2, 17, 19,  0), 123, 315),
+    (datetime(2020, 2, 17, 20,  0), 184, 389),
+    (datetime(2020, 2, 17, 21,  0), 260, 451),
+    (datetime(2020, 2, 17, 22,  0), 314, 484),
+    (datetime(2020, 2, 17, 22, 40), 392, 530),
+    (datetime(2020, 2, 17, 23, 52), 412, 540),
+    (datetime(2020, 2, 17, 23, 55), 455, 558),
+    (datetime(2020, 2, 18,  0, 30), 535, 593),
+    (datetime(2020, 2, 18,  1, 20), 566, 616),
+    (datetime(2020, 2, 18,  2, 10), 646, 644),
+    (datetime(2020, 2, 18,  3, 20), 736, 680)
+]
+
+# date, hits * 1e3, mis * 1e3
 long_term_raw_data = [
     (datetime(2020, 2, 18,  4, 0),  774,  711),
     (datetime(2020, 2, 18,  6, 0),  878,  757),
@@ -42,7 +64,12 @@ long_term_raw_data = [
 ]
 '''
 
-long_term_size = len(long_term_raw_data)
+short_term_size = len(short_term_raw_data)
+long_term_size  = len(long_term_raw_data)
+size            = short_term_size + long_term_size
+
+raw_data = short_term_raw_data + long_term_raw_data
+
 t0 = datetime(2020, 2, 17, 15, 18)
 
 
@@ -50,29 +77,33 @@ def to_hours(dt):
     return dt.days*24 + dt.seconds/3600
 
 
-time = [to_hours(r[0] - t0) / 10.0 for r in long_term_raw_data]
-hits = [r[1] / 1000.0 for r in long_term_raw_data]
-miss = [r[2] / 1000.0 for r in long_term_raw_data]
-n    = [hits[i] + miss[i] for i in np.arange(long_term_size)]
+long_term_time = [to_hours(r[0] - t0) / 10.0 for r in long_term_raw_data]
+long_term_hits = [r[1] / 1000.0 for r in long_term_raw_data]
+long_term_miss = [r[2] / 1000.0 for r in long_term_raw_data]
+long_term_n    = [long_term_hits[i] + long_term_miss[i] for i in np.arange(long_term_size)]
 
-A = np.vstack([time, np.ones(len(time))]).T
-lambda_h_start_0, H_star_0 = np.linalg.lstsq(A, hits, rcond=None)[0]
+time = [to_hours(r[0] - t0) / 10.0 for r in raw_data]
+hits = [r[1] / 1000.0 for r in raw_data]
+miss = [r[2] / 1000.0 for r in raw_data]
+n    = [hits[i] + miss[i] for i in np.arange(size)]
+
+A = np.vstack([long_term_time, np.ones(len(long_term_time))]).T
+lambda_h_start_0, H_star_0 = np.linalg.lstsq(A, long_term_hits, rcond=None)[0]
 print("lambda_h_start_0 = {0:.4}, H_star_0 = {1:.2}".format(lambda_h_start_0, H_star_0))
 
-A = np.vstack([time, np.ones(len(time))]).T
-lambda_s_start_0, S_star_0 = np.linalg.lstsq(A, miss, rcond=None)[0]
+A = np.vstack([long_term_time, np.ones(len(long_term_time))]).T
+lambda_s_start_0, S_star_0 = np.linalg.lstsq(A, long_term_miss, rcond=None)[0]
 print("lambda_s_start_0 = {0:.4}, S_star_0 =  {1:.2}".format(lambda_s_start_0, S_star_0))
 
 A = np.vstack([time, np.ones(len(time))]).T
 lambda_n, n_0 = np.linalg.lstsq(A, n, rcond=None)[0]
-print("lambda_n         = {0:.4},  n_0 =       {1:.2}".format(lambda_n, n_0))
+print("lambda_n         = {0:.4},  n_0      =  {1:.2}".format(lambda_n, n_0))
 
-time_origin = [0.0] + time
+plt.plot(time, hits, 'ro', color ='b')
+plt.plot(time, miss, 'ro', color ='g')
+plt.plot(time, n, 'bs', color ='r')
 
-plt.plot(time, hits, 'ro', color = 'b')
-plt.plot(time, miss, 'ro', color = 'g')
-plt.plot(time,    n, 'bs', color = 'r')
-
+time_origin = [0.0] + long_term_time
 plt.plot(time_origin, [lambda_h_start_0 * t + H_star_0 for t in time_origin])
 plt.plot(time_origin, [lambda_s_start_0 * t + S_star_0 for t in time_origin])
 plt.plot(time_origin, [lambda_n * t + n_0 for t in time_origin])
