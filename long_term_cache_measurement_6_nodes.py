6e71ca79270543b75f6d1a81b3c2ee1d4db41edb
oleksiyprosyanko <oleksii.prosianko@fedex.com>
2020-03-29 19:40:08 +0200

    long_term_cache_measurement_6_nodes:  - used split raw data instead of separate long  tesm and short ter data  - excluded `wrong` data

--- long_term_cache_measurement_6_nodes.py
+++ long_term_cache_measurement_6_nodes.py
@@ -3,32 +3,28 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # date, hits * 1e3, mis * 1e3
-short_term_raw_data = [
-    (datetime(2020, 2, 17, 15,  9),   0,   1),
-    (datetime(2020, 2, 17, 15, 18),   1,  18),
-    (datetime(2020, 2, 17, 15, 30),   4,  42),
-    (datetime(2020, 2, 17, 16,  0),   8,  72),
-    (datetime(2020, 2, 17, 16, 30),  17, 119),
-    (datetime(2020, 2, 17, 17,  0),  30, 155),
-    (datetime(2020, 2, 17, 17, 30),  48, 195),
-    (datetime(2020, 2, 17, 18,  0),  69, 235),
-    (datetime(2020, 2, 17, 19,  0), 123, 315),
-    (datetime(2020, 2, 17, 20,  0), 184, 389),
-    (datetime(2020, 2, 17, 21,  0), 260, 451),
-    (datetime(2020, 2, 17, 22,  0), 314, 484),
-    (datetime(2020, 2, 17, 22, 40), 392, 530),
-    (datetime(2020, 2, 17, 23, 52), 412, 540),
-    (datetime(2020, 2, 17, 23, 55), 455, 558),
-    (datetime(2020, 2, 18,  0, 30), 535, 593),
-    (datetime(2020, 2, 18,  1, 20), 566, 616),
-    (datetime(2020, 2, 18,  2, 10), 646, 644),
-    (datetime(2020, 2, 18,  3, 20), 736, 680),
-    (datetime(2020, 2, 18,  4, 0),  774, 711),
-    (datetime(2020, 2, 18,  5, 0),  817, 731)
-]
-
-# date, hits * 1e3, mis * 1e3
-long_term_raw_data = [
+raw_data = [
+    (datetime(2020, 2, 17, 15,  9),   0,    1),
+    (datetime(2020, 2, 17, 15, 18),   1,   18),
+    (datetime(2020, 2, 17, 15, 30),   4,   42),
+    (datetime(2020, 2, 17, 16,  0),   8,   72),
+    (datetime(2020, 2, 17, 16, 30),  17,  119),
+    (datetime(2020, 2, 17, 17,  0),  30,  155),
+    (datetime(2020, 2, 17, 17, 30),  48,  195),
+    (datetime(2020, 2, 17, 18,  0),  69,  235),
+    (datetime(2020, 2, 17, 19,  0), 123,  315),
+    (datetime(2020, 2, 17, 20,  0), 184,  389),
+    (datetime(2020, 2, 17, 21,  0), 260,  451),
+    (datetime(2020, 2, 17, 22,  0), 314,  484),
+    (datetime(2020, 2, 17, 22, 40), 392,  530),
+    # excluded (datetime(2020, 2, 17, 23, 52), 412,  540),
+    (datetime(2020, 2, 17, 23, 55), 455,  558),
+    (datetime(2020, 2, 18,  0, 30), 535,  593),
+    (datetime(2020, 2, 18,  1, 20), 566,  616),
+    (datetime(2020, 2, 18,  2, 10), 646,  644),
+    (datetime(2020, 2, 18,  3, 20), 736,  680),
+    (datetime(2020, 2, 18,  4, 0),  774,  711),
+    (datetime(2020, 2, 18,  5, 0),  817,  731),
     (datetime(2020, 2, 18,  6, 0),  878,  757),
     (datetime(2020, 2, 18,  8, 0), 1036,  826),
     (datetime(2020, 2, 18, 10, 0), 1231,  901),
@@ -43,34 +39,16 @@ long_term_raw_data = [
     (datetime(2020, 2, 19,  1, 0), 2294, 1457),
     (datetime(2020, 2, 19,  2, 0), 2388, 1484)]
 
-''' below cache size exeeds
-
-    (datetime(2020, 2, 19,  4, 0), 2573, 1520),
-    (datetime(2020, 2, 19,  6, 0), 2698, 1551),
-    (datetime(2020, 2, 19,  8, 0), 2935, 1596),
-    (datetime(2020, 2, 19, 10, 0), 3094, 1629),
-    (datetime(2020, 2, 19, 12, 0), 3301, 1678),
-    (datetime(2020, 2, 19, 14, 0), 3550, 1781),
-    (datetime(2020, 2, 19, 16, 0), 3673, 1856),
-    (datetime(2020, 2, 19, 18, 0), 3818, 1967),
-    (datetime(2020, 2, 19, 20, 0), 3930, 2108),
-    (datetime(2020, 2, 19, 22, 0), 4045, 2251),
-    (datetime(2020, 2, 20,  0, 0), 4169, 2369),
-    (datetime(2020, 2, 20,  2, 0), 4278, 2438),
-    (datetime(2020, 2, 20,  4, 0), 4453, 2530),
-    (datetime(2020, 2, 20,  6, 0), 4614, 2607),
-    (datetime(2020, 2, 20,  8, 0), 4782, 2683),
-    (datetime(2020, 2, 20, 10, 0), 4959, 2785),
-    (datetime(2020, 2, 20, 12, 0), 5103, 2876)
-]
-'''
 nodes = 6
-short_term_raw_data = [(date, nodes * hit, nodes * miss) for (date, hit, miss) in short_term_raw_data]
-long_term_raw_data  = [(date, nodes * hit, nodes * miss) for (date, hit, miss) in long_term_raw_data]
+split = 13
+
+short_term_raw_data = [(date, nodes * hit, nodes * miss) for (date, hit, miss) in raw_data[:split]]
+long_term_raw_data  = [(date, nodes * hit, nodes * miss) for (date, hit, miss) in raw_data[split:]]
 
 short_term_size = len(short_term_raw_data)
 long_term_size  = len(long_term_raw_data)
-size            = short_term_size + long_term_size
+size            = len(raw_data)
+assert size == short_term_size + long_term_size
 
 raw_data = short_term_raw_data + long_term_raw_data
 
